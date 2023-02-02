630c5e6ee059450526e1609eac8a0b789ae69209
isamauny <isabelle.mauny@42crunch.com>
2023-02-02 22:53:27 +0100

    added picture upload and delete

--- app/api-server.js
+++ app/api-server.js
@@ -12,7 +12,6 @@ var fs = require('fs');
 
 //database
 const MongoClient = require('mongodb').MongoClient;
-
 const dotenv = require('dotenv');
 
 //file uploading helpers
@@ -37,11 +36,10 @@ api.use(bodyParser.urlencoded({
 	extended: true
 }));
 
-api.use(cookieParser());
+//api.use(cookieParser());
 
 //accept files in /uploads dir (pictures)
 api.use(serveStatic(__dirname + '/uploads'));
-//api.use(serveStatic(__dirname + '/public'));
 
 //API binds to interface pixiapp:8090
 api.listen(8090, function () {
@@ -51,13 +49,11 @@ api.listen(8090, function () {
 });
 
 // Connect to MongoDB
-
 dotenv.config();
-//const mongo_url = process.env.MONGO_URL;
-const mongo_url = "mongodb://pixidb:27017";
+const mongo_url = process.env.MONGO_URL;
 console.log('API Server starting - Will connect to Mongo on: ' + mongo_url);
 
-// Mongo V3 Driver separates url from dbname / uses client
+// Mongo V3+ Driver separates url from dbname / uses client
 const db_name = 'Pixidb'
 let db
 
@@ -75,8 +71,8 @@ function api_authenticate(user, pass, req, res) {
 
 	users.findOne({ email: user, password: pass }, function (err, result) {
 		if (err) {
-			console.log('>>> Query error...');
-			return err;
+			console.log('>>> Query error...' + err);
+			res.status(500).json({ "message": "system error" });
 		}
 		if (result !== null) {
 			// API10: This is bad logging, as it dumps the full user record
@@ -88,25 +84,27 @@ function api_authenticate(user, pass, req, res) {
 			var token = jwt.sign(payload, privateKey, {
 				algorithm: 'RS384',
 				issuer: 'https://issuer.42crunch.demo',
-				subject: user,
+				subject: user_profile.email,
 				expiresIn: "30m",
 				audience: 'pixiUsers'
 			});
 
-			res.json({ message: "Token is a header JWT", token: token });
+			res.status(200).json({ message: "success", token: token, _id: user_profile._id });
 		}
 		else
-			res.status(401).json({ message: 'sorry pal, invalid login' });
+			res.status(401).json({ message: "sorry dear, invalid login" });
 	});
 }
 
 function api_register(user, pass, req, res) {
 	console.log('>>> Registering user: ' + user + ' with password: ' + pass);
-
 	const users = db.collection('users');
 	// Check if user exists first
 	users.findOne({ email: user }, function (err, result) {
-		if (err) { return err; }
+		if (err) {
+			console.log('>>> Query error...' + err);
+			res.status(500).json({ "message": "system error" });
+		}
 		if (result !== null) {
 			// Bad message: the error message should not indicate what the error is.
 			res.status(400).json({ "message": "user is already registered" });
@@ -134,25 +132,33 @@ function api_register(user, pass, req, res) {
 				name: name,
 				account_balance: req.body.account_balance,
 				is_admin: admin,
-				all_pictures: [],
 				onboarding_date: new Date()
 			};
 			// forceServerObjectId forces Mongo to use the specified _id instead of generating a random one.
 			users.insertOne(payload, { forceServerObjectId: true }, function (err, user) {
-				if (err) { res.status(500).json(err); return; }
-				if (user != null) {
+				if (err) {
+					console.log('>>> Query error...' + err);
+					res.status(500).json({ "message": "system error" });
+				}
+				if (user.insertedId != null) {
 					var user_profile = payload;
 					var jwt_payload = { user_profile };
-					var token = jwt.sign(jwt_payload, privateKey, {
-						algorithm: 'RS384',
-						issuer: 'https://42crunch.com',
-						subject: subject,
-						expiresIn: "30m",
-						audience: 'pixiUsers'
-					});
-					res.status(200).json({ message: "x-access-token", token: token, _id: payload._id });
+					var token = "";
+					try {
+						token = jwt.sign(jwt_payload, privateKey, {
+							algorithm: 'RS384',
+							issuer: 'https://issuer.42crunch.demo',
+							subject: subject,
+							expiresIn: "30m",
+							audience: 'pixiUsers'
+						})
+						res.status(200).json({ message: "success", token: token, _id: payload._id });
+					}
+					catch {
+						console.log(">>> Error occurred during JWT creation");
+						res.status(400).json({ message: "registration failure", token: null, _id: null });
+					}
 				} //if user
-
 			}) //insert
 		} // else
 	});
@@ -160,7 +166,7 @@ function api_register(user, pass, req, res) {
 
 function api_token_check(req, res, next) {
 
-	console.log('>>> Inbound token: ' + JSON.stringify(req.headers['x-access-token']));
+	console.log('>>> Validating token: ' + JSON.stringify(req.headers['x-access-token']));
 	var token = req.headers['x-access-token'];
 
 	// decode jwt token
@@ -169,7 +175,7 @@ function api_token_check(req, res, next) {
 		jwt.verify(token, publicKey, function (err, user) {
 			if (err) {
 				console.log(err)
-				return res.json({ success: false, message: 'Failed to authenticate token' });
+				return res.status(403).json({ success: false, message: 'Failed to authenticate token' });
 			} else {
 				// if everything is good, save to request for use in other routes
 				req.user = user;
@@ -190,37 +196,37 @@ function api_token_check(req, res, next) {
 
 function random_sentence() {
 	var samples = ["This day was {{ adjective }} and {{ adjective }} for {{ noun }}",
-		"The {{ nouns }} {{ adjective }} back! #YOLO",
-		"Today's breakfast, {{ an_adjective }}, {{ adjective }} for {{ noun }} #instafood",
-		"Oldie but goodie! {{ a_noun }} and {{ a_noun }} {{ adjective }} {{ noun }} #TBT",
-		"My {{ noun }} is {{ an_adjective }}, {{ adjective}} and {{ adjective }} which is better than yours #IRL #FOMO ",
-		"That time when your {{ noun }} feels {{ adjective }} and {{ noun }} #FML"
+		"The {{ nouns }} {{ adjective }} is back",
+		"Breakfast, {{ an_adjective }}, {{ adjective }} for {{ noun }}",
+		"Oldie but goodie {{ a_noun }} and {{ a_noun }} {{ adjective }} {{ noun }}",
+		"My {{ noun }} is {{ an_adjective }} which is better than yours",
+		"That time when your {{ noun }} feels {{ adjective }} and {{ noun }}"
 	];
 
-
 	var my_sentence = samples[Math.floor(Math.random() * (4 - 1)) + 1];
-
 	var sentencer = Sentencer.make(my_sentence);
 	return sentencer;
 }
 
-api.delete('/api/picture/:pictureid', api_token_check, function (req, res) {
-	console.log('>>> Deleting picture ' + req.params.pictureid);
+api.delete('/api/picture/:id', api_token_check, function (req, res) {
+	console.log('>>> Deleting picture ' + req.params.id);
 	const pictures = db.collection('pictures');
 	// BOLA - API1 Issue here: a user can delete someone's else picture.
 	// Code does not validate who the picture belongs too.
-	pictures.remove({ _id: Number(req.params.pictureid) },
-		function (err, delete_photo) {
-			if (err) { return err }
-			//console.log(delete_photo);
-			if (!delete_photo) {
-				res.status(400).json({ "message": "photo not found" });
+	pictures.deleteOne({ _id: req.params.id },
+		function (err, result) {
+			if (err) {
+				console.log('>>> Query error...' + err);
+				res.status(500).json({ "message": "system error" });
+			}
+			if (result.deletedCount == 0) {
+				console.log (">>> No picture was deleted")
+				res.status(400).json({ "message": "bad input" });
 			}
 			else {
-				console('Photo ' + req.params.pictureid + ' deleted');
-				res.status(202).json({ "message": "success" });
+				console.log ('>>> Photo ' + req.params.id + ' was deleted');
+				res.status(200).json({ "message": "success" });
 			}
-			db.close();
 		})
 });
 
@@ -233,11 +239,15 @@ api.delete('/api/admin/user/:id', api_token_check, function (req, res) {
 	else {
 		// API2 : Authorization issue - This call should enforce admin role, but it does not.
 		users.deleteOne({ _id: req.params.id },
-			function (err, delete_user) {
-				if (err) { res.status(500).json({ "message": "system error" }); }
-				//console.log(delete_user);
-				if (!delete_user) {
-					res.status(400).json({ "message": "bad request" });
+			function (err, result) {
+				if (err) {
+					console.log('>>> Query error...' + err);
+					res.status(500).json({ "message": "system error" });
+				}
+				//console.log("result object:" + result.deletedCount);
+				if (result.deletedCount == 0) {
+					console.log (">>> No user was deleted")
+					res.status(400).json({ "message": "bad input" });
 				}
 				else {
 					res.status(200).json({ "message": "success" });
@@ -247,17 +257,17 @@ api.delete('/api/admin/user/:id', api_token_check, function (req, res) {
 	}
 });
 
-api.post('/api/picture/upload', api_token_check, upload.single('file'), function (req, res, next) {
+api.post('/api/picture/upload', api_token_check, upload.single('filename'), function (req, res, next) {
 
-	const counters = db.collection("counters");
+	//const counters = db.collection("counters");
 	const pictures = db.collection("pictures")
 
 	if (!req.file) {
-		res.json({ message: "error: no file uploaded" });
+		res.status(400).json({ "message": "missing file" });
 	}
 	else {
 		console.log(">>> Uploading File: " + req.file);
-		console.log(">>> File name: "+ req.file.originalname)
+		console.log(">>> File name: " + req.file.originalname)
 		var description = random_sentence();
 		var name = randomWords({ exactly: 2 });
 		name = name.join(' ');
@@ -275,15 +285,18 @@ api.post('/api/picture/upload', api_token_check, upload.single('file'), function
 			created_date: new Date()
 		}
 
-		pictures.insertOne(payload, { forceServerObjectId: true }, function (err, photo) {
-			if (err) { res.status(500).json(err); return; }
-			if (photo !== null) {
-				res.status(200).json(photo);
+		pictures.insertOne(payload, { forceServerObjectId: true }, function (err, result) {
+			if (err) {
+				console.log('>>> Query error...' + err);
+				res.status(500).json({ "message": "system error" });
+			}
+			//console.log ("Inserted ID: " + result.insertedId)
+			if (result.insertedId !== null) {
+				res.status(200).json({"message": "success", "_id": result.insertedId});
 			}
 		}); // photo insert
 	} //else
 });
-
 
 // user related.
 api.post('/api/user/login', function (req, res) {
@@ -333,73 +346,51 @@ api.put('/api/user/edit_info', api_token_check, function (req, res) {
 	// Major issue here (API 6) - anyone can make themselves an admin!
 	if (req.body.hasOwnProperty('is_admin')) {
 		let is_admin_status = Boolean(req.body.is_admin);
-		objForUpdate.is_admin = is_admin_status 
+		objForUpdate.is_admin = is_admin_status
 	}
 	if (!req.body.email && !req.body.password && !req.body.name && !req.body.is_admin) {
-		res.status(422).json({ "message": "no data to update, add it to body" });
+		res.status(422).json({ "message": "Bad input" });
 	}
 	else {
 		var setObj = { objForUpdate }
-		console.log(JSON.stringify(setObj));
-		console.log(JSON.stringify(setObj));
-		console.log(JSON.stringify(setObj));
-		console.log(JSON.stringify(setObj));
-		// deepcode ignore NoSqli: test
- 		 users.findOneAndUpdate(
-			{ _id: req.user.user_profile._id }, 
-			{ $set: objForUpdate }, 
+		console.log(">>> Update User Data: " + JSON.stringify(setObj));
+		users.findOneAndUpdate(
+			{ _id: req.user.user_profile._id },
+			{ $set: objForUpdate },
 			{ returnNewDocument: true, upsert: true },
 			function (err, userupdate) {
-				if (err) { return err }
+				if (err) {
+					console.log('>>> Query error...' + err);
+					res.status(500).json({ "message": "system error" });
+				}
 				if (userupdate) {
 					console.log(userupdate);
 					res.status(200).json({ "message": "User Successfully Updated" });
+				}
+				else {
+					res.status(400).json({ "message": "Bad Request" });
 				}
 			})
 	}
 });
 
 api.get('/api/user/pictures', api_token_check, function (req, res) {
-	db.collection('pictures').find({ creator_id: req.user.user._id }).toArray(function (err, pictures) {
-		if (err) { return err };
+	
+	const pictures = db.collection('pictures');
+	
+	pictures.find({ creator_id: req.user.user_profile._id }).toArray(function (err, pictures) {
+		if (err) {
+			console.log('>>> Query error...' + err);
+			res.status(500).json({ "message": "system error" });
+		}
 
 		if (pictures) {
-			console.log(pictures);
+			console.log(">>> Pictures list: " + pictures);
 			res.json(pictures);
 
 		}
 	})
 });
-
-
-api.get('/api/user/likes', api_token_check, function (req, res) {
-	console.log('like id ' + req.user._id);
-	db.collection('likes').find({ user_id: req.user.user._id }).toArray(function (err, likes) {
-		if (err) { return err };
-
-		if (likes) {
-			console.log(likes);
-			res.json(likes);
-
-		}
-	})
-});
-
-api.get('/api/user/loves', api_token_check, function (req, res) {
-	console.log('love id ' + req.user.user._id);
-	mongo.connect(dbname, function (err, db) {
-		db.collection('loves').find({ user_id: req.user.user._id }).toArray(function (err, loves) {
-			if (err) { return err };
-
-			if (loves) {
-				console.log(loves);
-				res.json(loves);
-
-			}
-		})
-	})
-});
-
 
 api.get('/api/admin/all_users', api_token_check, function (req, res) {
 	//res.json(req.user);
@@ -411,19 +402,3 @@ api.get('/api/admin/all_users', api_token_check, function (req, res) {
 		}
 	})
 });
-
-//API ROUTES
-
-api.post('/api/login', function (req, res) {
-	api_authenticate(req.body.user, req.body.pass, req, res);
-})
-
-api.post('/api/register', function (req, res) {
-	if (req.body.user && req.body.pass) {
-		api_register(req.body.user, req.body.pass, req, res);
-	}
-	else {
-		res.status(202).json("missing user or pass");
-	}
-})
-
